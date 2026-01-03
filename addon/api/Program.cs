ca026361c7fd2963e7f640adea491408e0b54484
Sindarion <dmitrijs.skvorcovs92@gmail.com>
2026-01-03 11:27:17 +0200

    test

--- addon/api/Program.cs
+++ addon/api/Program.cs
@@ -1,4 +1,5 @@
 using api.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,7 +33,12 @@ else
     Console.WriteLine("FILE EXISTS!!!!");
 }
 
-    builder.Services.AddControllers();
+var optionsJson = File.ReadAllText(optionsPath);
+var haOptions = JsonSerializer.Deserialize<AppConfiguration>(optionsJson);
+
+Console.WriteLine($"DB from HA options: {haOptions.db}");
+
+builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
