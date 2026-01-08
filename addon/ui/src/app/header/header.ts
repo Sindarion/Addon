01399a3d962419d6bb20506dd1bac8bc785184d6
Sindarion <dmitrijs.skvorcovs92@gmail.com>
2026-01-08 22:47:29 +0200

    menu

---
+++ addon/ui/src/app/header/header.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Menubar } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-header',
+  imports: [Menubar],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header implements OnInit {
+
+  public items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Tasks',
+        icon: 'pi pi-search',
+        items: [
+          {
+            label: 'To Do',
+            icon: 'pi pi-hammer'
+          },
+          {
+            label: 'To Buy',
+            icon: 'pi pi-cart-plus'
+          }
+        ]
+      },
+      {
+        label: 'Expenses',
+        icon: 'pi pi-euro'
+      },
+      {
+        label: 'Administration',
+        icon: 'pi pi-cog'
+      }
+    ]
+  }
+
+}
